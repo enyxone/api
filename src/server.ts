805fc070f65725b9178e700e5142a531856f3df4
enyxone <xyloblonk@gmail.com>
2026-03-26 09:31:32 -0400

    Update server.ts
    Signed-off-by: enyxone <xyloblonk@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
+
 import app from "./app";
 
 const PORT = process.env.PORT || 8080;
-
 app.listen(PORT, () => {
-  console.info(`Secure API listening on port ${PORT}`);
+  console.log(`API running on ${PORT}`);
 });
-``
